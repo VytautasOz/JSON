05018d13b4c76507495e5d1de7874d89079afa24
VytautasOz <patrikasv@gmail.com>
2025-01-15 21:37:35 +0200

    Json Uzduotis 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Person person = new Person("Vytas",50,"lol@gmail.com");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        File output = new File("src/main/java/org/example/rezultatas.json");
+        objectMapper.writeValue(output, person);
+
+        File input = new File("src/main/java/org/example/rezultatas.json");
+        Person personJson = objectMapper.readValue(input, Person.class);
+
+        System.out.println(person.getName() + person.getAge() + person.getEmail());
+        System.out.println(personJson.getName() + personJson.getAge() + personJson.getEmail());
+
+
     }
 }
